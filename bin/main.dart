603e880e3c0dddd372a1e7834ca70a622923f9da
Rechunk <gabri.marc@gmx.net>
2017-05-28 12:10:03 +0200

    Proper lexing

--- bin/main.dart
+++ bin/main.dart
@@ -1,74 +1,79 @@
 import 'dart:io';
-import 'dart:convert';
+import "dart:core";
 
-List<String> checkedLines = [];
-List<String> sortedLines = [];
-List<String> allProperties = [];
-int i = 0;
-
-main() {
-  var path = "C:/Users/User/Desktop/Datein/AlphaCSS/bin/samplecss.txt";
-
-
-  new File(path)
-    .openRead()
-    .transform(UTF8.decoder)
-    .transform(new LineSplitter())
-    .forEach((line) => identifyLineType(line)).then(performActionBasedOnType);
+class Token {
+  String identifier;
+  String value;
+  Token(this.identifier, this.value);
 }
 
-performActionBasedOnType(thisisnull){
-  while (i < checkedLines.length){
-    switch(checkedLines[i].substring(0, 5)){
-      case "NORM:":
-        sortedLines.add(checkedLines[i]);
-        break;
-      case "PROP:":
-        getAllPropertiesInCurrentSelector();
-        sortProperties();
-        break;
+int i = 0;
+
+void main() {
+  var path = "C:/Users/User/Desktop/Datein/AlphaCSS/bin/samplecss.txt";
+
+  new File(path).readAsString().then((String file) {
+    List<Token> tokens = getTokensFromFile(file);
+    List<Token> sortedTokens = getSortedTokens(tokens);
+    for (var x in sortedTokens){
+      print("${x.identifier} ${x.value}");
+    }
+  });
+}
+
+bool isValidAccessor(String char){
+  RegExp regex = new RegExp(r"[a-zA-Z0-9#.]");
+  return (regex.hasMatch(char)) ? true : false;
+}
+
+List<Token> getTokensFromFile(file){
+  List<Token> tokens = [];
+  String value = "";
+  while (i < file.length-1){
+    value = "";
+    if (isValidAccessor(file[i])){
+      while (file[i] != "{" && file[i] != ";"){
+        value += file[i];
+        i++;
+      }
+      if (file[i] == "{"){
+        tokens.add(new Token("ACCESSOR", value));
+      }
+      else {
+        tokens.add(new Token("PROPERTY", value));
+      }
     }
     i++;
   }
-  reconstructCSS();
+  return tokens;
 }
 
 void reconstructCSS() {
-  String finalCss = "";
-  for (var line in sortedLines){
-    finalCss += line.substring(5) + "\n";
-  }
-  print(finalCss);
+
 }
 
-void sortProperties(){
-  allProperties.sort();
-  for (var property in allProperties){
-    sortedLines.add(property);
+List<Token> getSortedTokens(tokens){
+  List<Token> sortedTokens = [];
+  i = 0;
+  try {
+    while (i < tokens.length-1){
+      if (tokens[i].identifier == "PROPERTY"){
+        List<Token> toSort = [];
+        while (tokens[i].identifier == "PROPERTY"){
+          toSort.add(tokens[i]);
+          i++;
+        }
+        toSort.sort((current, next) => current.value.compareTo(next.value));
+        for (var elem in toSort){
+          sortedTokens.add(elem);
+        }
+      }
+      else if (tokens[i].identifier == "ACCESSOR"){
+        sortedTokens.add(tokens[i]);
+      }
+      i++;
+    }
   }
-  allProperties.clear();
-}
-
-getAllPropertiesInCurrentSelector() {
-  while (checkedLines[i].substring(0, 5) == "PROP:"){
-    allProperties.add(checkedLines[i]);
-    i++;
-  }
-  i--;
-}
-
-bool isProperty(line) {
-  if (line.contains(";")){
-    return true;
-  }
-  return false;
-}
-
-void identifyLineType(line){
-  if (isProperty(line)){
-    checkedLines.add("PROP:$line");
-  }
-  else {
-    checkedLines.add("NORM:$line");
-  }
+  catch(ex) {}
+  return sortedTokens;
 }
